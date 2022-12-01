820e6d1fa2ae348c78e634dc12d1c1d7f783f5ad
Raif Cinar <raifcinar81@gmail.com>
2022-12-01 11:22:52 +0300

    added - hypotenuse, perimeter, and area calculator in the right triangle.

---
+++ Java101/AreaAndHypotenuseCalculatorInTheRightTriangle/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            Program to Find Hypotenuse in Right Triangle
+            Write a program in Java that takes the length of the perpendicular
+        sides from the user and calculates the hypotenuse.
+
+            Homework
+            Write a program that calculates the area of the triangle whose three side lengths
+        you get from the user.
+            e1 and e2: perpendicular sides
+            h: hypotenuse
+            a: area
+            p: perimeter length
+
+            Formulas to be used:
+            perimeter: p = e1 + e2 + h;
+            area : e1 * e2 / 2;
+            hypotenuse: h = √((e1*e1)+(e2*e2))
+
+            NOTE: The two side lengths are taken and the calculated hypotenuse,
+        perimeter, and area values are printed.
+            NOTE: No exception checks have been made.
+        */
+
+        Scanner input = new Scanner(System.in);
+        double e1, e2, h, a, p;
+
+        System.out.println("Please write the 1st edge length of the triangle. (not hypotenuse)");
+        e1 = input.nextFloat();
+        System.out.println("Please write the 2nd edge length of the triangle. (not hypotenuse)");
+        e2 = input.nextFloat();
+
+        h = Math.sqrt((e1*e1) + (e2*e2));
+        p = e1 + e2 + h;
+        a = e1 * e2 / 2;
+
+        System.out.println("For Right Triangle;"
+                + "\nHypotenus: " + h
+                + "\nPerimeter: " + p
+                + "\nArea: " + a);
+    }
+}
